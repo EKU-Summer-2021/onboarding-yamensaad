e76637d1a9cf2f84e6b02ca27a6d999eae694ef2
yamensaad <yamen.saa199@gmail.com>
2021-07-06 16:45:22 +0200

    fix : pandas was installed

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import numpy as np
-from src import Polynomial
+"""
+main class
+"""
+
 from src.read_csv import read_csv
 
-if __name__ == '__main__':
-    read_csv(
-        'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/CSP/CSP_53000.csv')
-
-    coeffs = np.array([1,0,0])
-    polynom = Polynomial(coeffs)
-    print(polynom.evaluate(3))
-    print(polynom.roots())
-
-
-
-
+print(read_csv('https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/'
+               'main/Intelligent%20System%20Data/KP/KP_10.csv'))
